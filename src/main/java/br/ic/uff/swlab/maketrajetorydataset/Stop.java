724e83d7aa61d9665fe0329c70d4abe512c6a166
Luiz Leme <lapaesleme@ic.uff.br>
2020-01-20 16:39:31 -0300

    ok

---
+++ src/main/java/br/ic/uff/swlab/maketrajetorydataset/Stop.java
@@ -0,0 +1,20 @@
+package br.ic.uff.swlab.maketrajetorydataset;
+
+public class Stop {
+
+    public Cluster cluster;
+    public int numPhotos;
+    public long begin;
+    public long end;
+
+    public Stop(Cluster clus, int numPhotos, long begin, long end) {
+        this.cluster = clus;
+        this.numPhotos = numPhotos;
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public String toString() {
+        return cluster + ", " + numPhotos + ", " + begin + ", " + end;
+    }
+}
